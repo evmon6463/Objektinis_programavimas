4a82fe2c5e048413703cfd48868392c458b82d1b
evmon6463 <evelina.monastyrska@mif.stud.vu.lt>
2020-09-19 14:47:15 +0300

    Vieno studento duomenų nuskaitymas ir išvedimas

--- main.cpp
+++ main.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
+#include <iomanip>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+using std::string;
+using std::cout;
+using std::endl;
+using std::cin;
+
+struct duomenys {
+    string Vardas;
+    string Pavarde;
+    int nd_rezultatai[10];
+    int egzamino_rezultatas;
+    float galutinis_rezultatas;
+};
+int main()
+{
+    duomenys studentas;
+    float suma = 0;
+    cout << "Iveskite studento varda, pavarde, namu darbu rezultatus ir egzamino rezultata: " << endl;
+    cin >> studentas.Vardas >> studentas.Pavarde;
+    for (int i = 0; i < 5; i++) {
+        cin >> studentas.nd_rezultatai[i];
+        suma = suma + studentas.nd_rezultatai[i];
+    }
+    float vidurkis = suma / 5;
+    cin >> studentas.egzamino_rezultatas;
+    studentas.galutinis_rezultatas = vidurkis * (float)0.4 + studentas.egzamino_rezultatas * (float)0.6;
+    cout << "Vardas:\t" << "Pavarde:\t"<< "Galutinis rezultatas:\t" << endl;
+    cout << "- - - - - - - - - - - - - - - - - - - - - - -"<<endl;
+    cout << studentas.Vardas<<"\t"<<studentas.Pavarde <<"\t\t"<<std::setprecision (2) << std::fixed<< studentas.galutinis_rezultatas<<"\t"<<endl;
 }
